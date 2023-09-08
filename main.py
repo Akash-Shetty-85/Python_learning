b2c10749b19df2066f449429d0f527ae68a79ccf
Akash <mr.shetty28502@gmail.com>
2023-09-08 09:35:17 +0530

    day18

---
+++ main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+t = Turtle()
+t.shape("turtle")
+t.color("red")
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
