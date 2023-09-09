925aef703eb22c1da8bc3d1bc3bc775c85fd029d
Akash <mr.shetty28502@gmail.com>
2023-09-09 09:05:15 +0530

    day18- random walk with random color

--- main.py
+++ main.py
@@ -14,17 +14,31 @@ t = Turtle()
 colors = ["dark green", "dark green", "firebrick", "violet", "orange red", "dark goldenrod", "light steel blue",
           "lawn green", "steel blue", "green yellow", "gold"]
 
+# def draw_shape(sides):
+#     angle = 360 / sides
+#     t.width(2)
+#     t.color(random.choice(colors))
+#     for _ in range(sides):
+#         t.forward(100)
+#         t.right(angle)
+#
+#
+# for i in range(3, 15):
+#     draw_shape(i)
 
-def draw_shape(sides):
-    angle = 360 / sides
-    t.width(2)
+directions = [0, 90, 180, 270]
+
+
+def random_walk():
+    choice_of_directions = random.choice(directions)
+    t.speed("fastest")
+    t.width(3)
     t.color(random.choice(colors))
-    for _ in range(sides):
-        t.forward(100)
-        t.right(angle)
+    t.forward(30)
+    t.setheading(choice_of_directions)
 
 
-for i in range(3, 15):
-    draw_shape(i)
+for _ in range(300):
+    random_walk()
 screen = Screen()
 screen.exitonclick()
